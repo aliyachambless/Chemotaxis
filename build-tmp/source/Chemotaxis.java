55d7ce1331a4f9af603de80a5b23e92da6fc683e
unknown <ALCHAMBLESS@697-334-Gimli.sf.k12.internal>
2015-09-14 09:53:13 -0700

    first working version

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
  //declare bacteria variables here   
  Bacteria cell;
  Bacteria [] colony;
- void setup()   
+ public void setup()   
  {     
  	//initialize bacteria variables here  
  	background(0,0,0);
@@ -12,7 +28,7 @@
  	}
  	size(500,500);
  }   
- void draw()   
+ public void draw()   
  {    
  	//move and show the bacteria
  	fill(0,0,0,10);
@@ -33,13 +49,22 @@
  		bacteriaColor = bacColor;
  	}  
  	//lots of java!   
- 	void move(){
+ 	public void move(){
  		xPos += ((int)(Math.random()*3)-1);
  		yPos += ((int)(Math.random()*3)-1);
  	}
- 	void show(){
+ 	public void show(){
  		noStroke();
  		fill(200,150,bacteriaColor);
  		ellipse(xPos, yPos, 10, 10);
  	}
  }    
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
